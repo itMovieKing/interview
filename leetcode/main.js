378df9c1e40abafca5710e48618e265eabc9662b
zhangxiaoyun <zhangxiaoyun@abakus.com>
2025-05-04 17:46:22 +0800

    [leetcode]: 88 done

--- leetcode/main.js
+++ leetcode/main.js
@@ -6,19 +6,21 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+/**
+ * 仅遍历num2就行，只需要在num1中找到对应位置插入num2的元素就行
+ */
 var merge = function(nums1, m, nums2, n) {
   let i = m + n - 1 // 总下标
   let j = m - 1 // nums1下标
   let k = n - 1 // nums2下标
-  while(i >= 0) {
-      if(nums1[j] > nums2[k]) {
+  while(k >= 0) {
+    
+      if(j >= 0 && nums1[j] > nums2[k]) {
           nums1[i] = nums1[j]
-          console.log('1 > 2',nums1)
           j--;
           i--;
       } else {
           nums1[i] = nums2[k];
-          console.log(nums1)
           k--;
           i--;
       }
